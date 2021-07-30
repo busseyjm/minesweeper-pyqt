c25e799e69664f9fdff4b97be273fd5b54edac5e
Joel Bussey <jbussey03@gmail.com>
2021-07-30 03:25:21 -0700

    Added assets, made large amounts of progress

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
 import sys
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRunnable, QThreadPool, QSize
 from minesweepergame import game
-from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox,
+from PyQt5.QtWidgets import (QAbstractButton, QApplication, QLabel, QWidget, QMessageBox,
 QPushButton, QGridLayout)
+from PyQt5.QtGui import QAbstractOpenGLFunctions, QIcon
 
 MINESWEEPER_GAME_SIZE_X = 30
 MINESWEEPER_GAME_SIZE_Y = 16
+MINESWEEPER_GAME_DIFFICULTY = "EXPERT"
+
+#multithreaded board update
+class BoardRefresh(QRunnable):
+    def __init__(self, buttons, board):
+        self.buttons = buttons
+        self.board = board
+        super().__init__()
+
+    def run(self):
+        # TODO refactor this to NOT update every square every cycle
+        # will make a list containing changed tuples, and only update them
+        for y, x in self.buttons:
+            if (self.board[y,x] == 'U'):
+                continue
+
+            if (self.board[y,x] == 'E'):
+                self.buttons[y,x].setIcon(QIcon("assets/minesweeper_01.png"))
+                continue
+
+            if (self.board[y,x] == 'F'):
+                self.buttons[y,x].setIcon(QIcon("assets/minesweeper_02.png"))
+                continue
+
+            if (self.board[y,x] == 'B'):
+                self.buttons[y,x].setIcon(QIcon("assets/minesweeper_05.png"))
+                continue
+
+            if (self.board[y,x] == 'C'):
+                self.buttons[y,x].setIcon(QIcon("assets/minesweeper_06.png"))
+                continue
+
+            if (self.board[y,x] == 'W'):
+                self.buttons[y,x].setIcon(QIcon("assets/minesweeper_07.png"))
+                continue
+
+            # somewhat hacky way of translating the number into a filename
+            #   the '1' icon is _08.png
+            #   the '8' icon is _15.png
+            iconstr = ""
+            if (self.board[y,x] in range(1,3)):
+                iconstr = "0" + str(self.board[y,x]+7)
+            else:
+                iconstr = str(self.board[y,x]+7)
+            self.buttons[y,x].setIcon(QIcon("assets/minesweeper_" + iconstr + ".png"))
+
 
 class mainboard(QWidget):
+    buttons = {}
     gameinst = game()
 
     def __init__(self):
@@ -24,19 +72,38 @@ class mainboard(QWidget):
             self.gameinst.clicked(pos_y, pos_x, True)
         else:
             self.gameinst.clicked(pos_y, pos_x, False)
+        #QApplication.processEvents()
+        self.refreshboard()
+
+    def refreshboard(self):
+        pool = QThreadPool.globalInstance()
+        refresh = BoardRefresh(self.buttons, self.gameinst.getboardfront())
+        pool.start(refresh)
 
     def initUI(self):   
         grid = QGridLayout()  
         self.setLayout(grid)
+        grid.setVerticalSpacing(0)
+        grid.setHorizontalSpacing(0)
 
         positions = [(i, j) for i in range(MINESWEEPER_GAME_SIZE_Y) for j in range(MINESWEEPER_GAME_SIZE_X)]
 
         for position in positions:
             button = QPushButton()
 
+            #button appearance settings
+            button.setMinimumWidth(32)
+            button.setMinimumHeight(32)
+            button.setFixedHeight(32)
+            button.setFixedWidth(32)
+            button.setFlat(True)
+            button.setIconSize(QSize(32,32))
+            button.setIcon(QIcon("assets/minesweeper_00.png"))
+
             #set a property containing the coordinates of the button
             button.setProperty("y_coord", position[0])
             button.setProperty("x_coord", position[1])
+            button.setProperty("coords", position)
 
             #connect to a click handler
             button.clicked.connect(self.clickhandler)
@@ -46,16 +113,17 @@ class mainboard(QWidget):
             button.customContextMenuRequested.connect(self.clickhandler)
 
             #add the button to the game
-            self.gameinst.addbutton(positions[0],positions[1],button)
+            #self.gameinst.addbutton(position[0],position[1],button)
+            self.buttons[position] = button
 
             #add the button to the ui
             grid.addWidget(button, *position)
 
         #self.gameinst.generategame(positions[0], positions[1], .5)
-        self.gameinst.generategame("EXPERT")
-        self.gameinst.calculatebackend()
+        self.gameinst.generategame(MINESWEEPER_GAME_DIFFICULTY)
         self.move(300, 150)
         self.setWindowTitle('PyQt Minesweeper')  
+        #self.setFixedSize(self.size())
         self.show()
 
     def closeEvent(self, event):
@@ -70,4 +138,5 @@ class mainboard(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = mainboard()
+    mainboard.gameinst.debugprint()
     sys.exit(app.exec_())    
--- minesweepergame.py
+++ minesweepergame.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 #coord system for this game:
 #using the basis of a 2d array
@@ -23,23 +24,28 @@ import random
 #   E       = empty and clicked
 #   1-8     = bomb count and clicked
 #   F       = flagged
-#   ?       = question marked
+#   B       = bomb
+#   C       = clicked bomb
+#   W       = wrongly flagged bomb
 
 random.seed(12345)
 
 class game():
-    buttons = {}
-    boardback = ""
-    boardfront = ""
-    bombs = ""
-    bombcount = ""
-    sizex = ""
-    sizey = ""
+    #front and back arrays, front is what the user sees
+    boardback = dict()
+    boardfront = dict()
 
-    def __init__(self):
-        buttons = {}
-        sizex = 0
-        sizey = 0
+    #coordinates of every bomb, in tuples
+    bombs = []
+
+    #how many bombs there are
+    bombcount = 0
+
+    #size of the game
+    sizex = 0
+    sizey = 0
+
+    #def __init__(self):
 
     #perhaps add a reference to the gridlayout, and generate the board here
     # requires a bit more knowledge of layouts
@@ -59,22 +65,26 @@ class game():
             self.sizex = 30
             self.sizey = 16
 
-        self.boardback = [['X']*self.sizex for i in range(self.sizey)]
-        self.boardback = [['U']*self.sizex for i in range(self.sizey)]
-        # for y in range(0,self.sizey):
-        #     for x in range(0,self.sizex):
-        #         print(str(x) + " " + str(y))
-        #         self.boardback[y][x] = 'E'
-        #         self.boardfront[y][x] = ' '
+        for y in range(0,self.sizey):
+            for x in range(0,self.sizex):
+                self.boardback[y,x] = 'X'
+                self.boardfront[y,x] = 'U'
 
+        # generate a sequence of unique numbers that are within the max square count
         bombs = random.sample(range(0,(self.sizex*self.sizey)), self.bombcount)
 
+        # translate the numbers into coordinates within the grid
         for i in bombs:
             bombx = i % self.sizex
+            i = math.floor(i/self.sizex)
             bomby = i % self.sizey
-            self.boardback[bomby][bombx] = 'B'
+            self.boardback[bomby,bombx] = 'B'
+            self.bombs.append((bomby,bombx))
         
+        self.calculatebackend()
+
     def generatecustomgame(self, height, width, percentbombs):
+        #TODO
         self.sizex = width
         self.sizey = height
         print(random.sample(range(1,100), 5))
@@ -82,149 +92,124 @@ class game():
     def calculatebackend(self):
         for y in range(0,self.sizey):
             for x in range(0,self.sizex):
-                if (self.boardback[y][x] == 'B'):
+                if (self.boardback[y,x] == 'B'):
                     continue
 
                 bombcount = 0
 
-                #corners: (0,0)(0,sizex-1)(sizey-1,0)(sizey-1,sizex-1)
-
-                #top left corner
-                if (y==0) and (x==0):
-                    if (self.boardback[y][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x+1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #top right corner
-                if (y==0) and (x==self.sizex-1):
-                    if (self.boardback[y][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x-1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #bottom left corner
-                if (y==self.sizey-1) and (x==0):
-                    if (self.boardback[y-1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x+1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-                    
-                #bottom right corner
-                if (y==self.sizey-1) and (x==self.sizex-1):
-                    if (self.boardback[y-1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x-1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #edge cases for borders (corners excluded):
-                #all x for y=0
-                #all x for y=sizey - 1
-                #all y for x=0
-                #all y for y = sizex - 1
-
-                #top border
-                if (y==0):
-                    if (self.boardback[y][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x+1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-                
-                #bottom border
-                if (y==self.sizey-1):
-                    if (self.boardback[y][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x+1] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #left border
-                if (x==0):
-                    if (self.boardback[y-1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x+1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #right border
-                if (x==self.sizex-1):
-                    if (self.boardback[y-1][x] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y-1][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x-1] == 'B'):
-                        bombcount += 1
-                    if (self.boardback[y+1][x] == 'B'):
-                        bombcount += 1
-                    self.boardback[y][x] = bombcount
-                    continue
-
-                #non edge case
-                if (self.boardback[y-1][x-1] == 'B'):
+                if (((y-1,x-1) in self.boardback) and self.boardback[y-1,x-1] == 'B'):
                     bombcount += 1     
-                if (self.boardback[y-1][x] == 'B'):
+                if (((y-1,x) in self.boardback) and self.boardback[y-1,x] == 'B'):
                     bombcount += 1 
-                if (self.boardback[y-1][x+1] == 'B'):
+                if (((y-1,x+1) in self.boardback) and self.boardback[y-1,x+1] == 'B'):
                     bombcount += 1 
-                if (self.boardback[y][x-1] == 'B'):
+                if (((y,x-1) in self.boardback) and self.boardback[y,x-1] == 'B'):
                     bombcount += 1 
-                # if (self.boardback[y][x] == 'B'):
+                # if (((y,x) in self.boardback) and  self.boardback[y,x] == 'B'):
                 #     bombcount += 1 
-                if (self.boardback[y][x+1] == 'B'):
+                if (((y,x+1) in self.boardback) and self.boardback[y,x+1] == 'B'):
                     bombcount += 1 
-                if (self.boardback[y+1][x-1] == 'B'):
+                if (((y+1,x-1) in self.boardback) and self.boardback[y+1,x-1] == 'B'):
                     bombcount += 1 
-                if (self.boardback[y+1][x] == 'B'):
+                if (((y+1,x) in self.boardback) and self.boardback[y+1,x] == 'B'):
                     bombcount += 1 
-                if (self.boardback[y+1][x+1] == 'B'):
+                if (((y+1,x+1) in self.boardback) and self.boardback[y+1,x+1] == 'B'):
                     bombcount += 1   
-                self.boardback[y][x] = bombcount
+                self.boardback[y,x] = bombcount
                 continue
-        print(self.boardback)
 
-
-    def addbutton(self, y, x, button):
-        self.buttons[y,x] = button
-
+    # recursive reveal function, called when clicking on a square that is not a bomb
+    # will reveal all 0s adjacent to the clicked square, and all numbered squares
+    #   adjacent to any 0 squares
+    def reveal(self,y,x):
+        #recursion base case, stop when it reaches a revealed square
+        if (self.boardfront[y,x] != 'U'):
+            return
+        #any square adjacent to a zero is either a 0 or a number
+        # show the number, recurse on the 0s
+        if (self.boardfront[y,x] == 'U'):
+            if (self.boardback[y,x] == 0):
+                self.boardfront[y,x] = 'E'
+                if ((y-1,x-1) in self.boardback):
+                    self.reveal(y-1,x-1)
+                if ((y-1,x) in self.boardback):
+                    self.reveal(y-1,x)
+                if ((y-1,x+1) in self.boardback):
+                    self.reveal(y-1,x+1)
+                if ((y,x-1) in self.boardback):
+                    self.reveal(y,x-1)
+                # if ((y,x) in self.boardback):
+                #     self.reveal(y,x)
+                if ((y,x+1) in self.boardback):
+                    self.reveal(y,x+1)
+                if ((y+1,x-1) in self.boardback):
+                    self.reveal(y+1,x-1)
+                if ((y+1,x) in self.boardback):
+                    self.reveal(y+1,x)
+                if ((y+1,x+1) in self.boardback):
+                    self.reveal(y+1,x+1)
+            if (self.boardback[y,x] in range(1,9)):
+                self.boardfront[y,x] = self.boardback[y,x]
+        
     def clicked(self, y, x, rightclick):
-        print("clicked " + str(y) + ", " + str(x) + str(rightclick))
+        print("clicked " + str(y) + ", " + str(x) + " " + str(rightclick))
+        print(self.boardback[y,x])
+
+        if (self.boardfront[y,x] == 'E'):
+            return
+
+        if (rightclick):
+            #flag and unflag the rightclicked square
+            if (self.boardfront[y,x] == 'F'):
+                self.boardfront[y,x] = 'U'
+            else:
+                self.boardfront[y,x] = 'F'
+            return
+
+        #unclicked buttons
+        if (self.boardfront[y,x] == 'U'):
+            if (self.boardback[y,x] == 0):
+                self.reveal(y,x)
+                self.debugprint()
+                return
+
+            if (self.boardback[y,x] in range(0,9)):
+                self.boardfront[y,x] = self.boardback[y,x]
+                return
+
+            if (self.boardback[y,x] == 'B'):
+                # TODO: lose game code here (clicked bomb, replace all unflagged unclicked bombs with bombs)
+                # until then just put a clicked bomb
+                self.boardfront[y,x] = 'C'
+                return
+
+        #clicked buttons (quick clearing by clicking numbers w/ flagged spaces adjacent)
+        # TODO    
+        
+
+    def getboardback(self):
+        return self.boardback
+
+    def getboardfront(self):
+        return self.boardfront
+
+    def debugprint(self):
+        print("Boardback:")
+        for y in range(0,self.sizey):
+            print("[", end="")
+            for x in range(0,self.sizex):
+                if (x!=self.sizex-1):
+                    print(self.boardback[y,x], end=", ")
+                else:
+                    print(self.boardback[y,x], end="")
+            print("]")
+
+        print("Boardfront:")
+        for y in range(0,self.sizey):
+            print("[", end="")
+            for x in range(0,self.sizex):
+                if (x!=self.sizex-1):
+                    print(self.boardfront[y,x], end=", ")
+                else:
+                    print(self.boardfront[y,x], end="")
+            print("]")
